61194dda0c19fc6207ccb5161492f7f447446cf0
amit373 <amit373>
2022-12-17 18:54:37 +0530

    feat: initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import dynamic from 'next/dynamic';
+
+import { connect } from '@/backend';
+import { Main, Meta } from '@/layouts';
+
+const HomeComponent = dynamic(() => import('../containers/Home'), {
+  ssr: true,
+});
+
+interface IProps {}
+
+const Index: React.FC<IProps> = (): JSX.Element => {
+  return (
+    <Main meta={<Meta title="Home Page" description="Home Page Description" />}>
+      <HomeComponent />
+    </Main>
+  );
+};
+
+export default Index;
+
+export async function getServerSideProps() {
+  await connect();
+  return {
+    props: {},
+  };
+}
---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,11 @@
+export const AppConfig = {
+  site_name: 'HRM - Thoughtwin',
+  locale: 'en',
+  title: 'Software development company|top mobile app development company.',
+  description: ` Thoughtwin is a leading web, mobile app, and software development firm in India and the Canada, offers business IT technology services and solutions.`,
+  tokenKey: 'token',
+  paths: {
+    BASE: process.env.NEXT_PUBLIC_BASE_PATH!,
+    WEBSITE_URL: process.env.NEXT_PUBLIC_BASE_PATH!,
+  },
+};
